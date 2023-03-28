b6543532b79d14863fb465f979de9c079192c540
William John Austin <wiaustin@my365.bellevue.edu>
2023-03-28 15:47:58 -0500

    minor tweaks for week 3

--- week-3/dessert.js
+++ week-3/dessert.js
@@ -4,7 +4,8 @@
     Author: William Austin
     Date: 3/28/2023
     Description: Creating a Dessert Class Module 
-*/import {Product} from "./products.js";
+*/
+import {Product} from "./products.js";
 
 export class Dessert extends Product {
     constructor (name, price){
--- week-3/main-course.js
+++ week-3/main-course.js
@@ -4,7 +4,8 @@
     Author: William Austin
     Date: 3/28/2023
     Description: Creating a Main Course Class Module 
-*/import {Product} from "./products.js";
+*/
+import {Product} from "./products.js";
 
 export class MainCourse extends Product{
     constructor (name, price){
