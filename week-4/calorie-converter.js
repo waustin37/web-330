99951417b009178cbba303289926b38db207adf6
William John Austin <wiaustin@my365.bellevue.edu>
2023-04-06 16:56:58 -0500

    Adding Week 4

---
+++ week-4/calorie-converter.js
@@ -0,0 +1,16 @@
+import {FoodModel} from "./food-model.js";
+
+export class CalorieConverter{
+    static data = [
+        new FoodModel(1007, "Egg", 78),
+        new FoodModel(1008, "Apple", 95),
+        new FoodModel(1009, "Hamburger", 354),
+        new FoodModel(1010, "Fries", 400),
+        new FoodModel(1011, "Banana", 105),
+        new FoodModel(1012, "Soda", 150),
+    ]
+    static find(string) {
+        return this.data.filter(foodModel => foodModel.name.toLowerCase().includes(string.toLowerCase()));
+    }
+}
+
---
+++ week-4/food-model.js
@@ -0,0 +1,16 @@
+/*
+    ==================
+    Title: food-model.js, 
+    Author: William Austin
+    Date: 4/06/2023
+    Description: Creating a Food Model Class Module for Calorie App
+*/
+
+export class FoodModel {
+    constructor(id, name, calories){
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+}
+
